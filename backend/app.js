a7aabcd1e7c94386733a52422c93e074843232e1
cdelst <casedelst@gmail.com>
2022-01-30 18:18:00 -0800

    Rework server and add location ingestion

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,63 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+var lastKnownLocationObject
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/put-location', (req, res) => {
+  if (req.header("Content-Type") !== "application/json") {
+    res.send("Bad request")
+    return
+  }
+
+  if (Object.keys(req.body).length === 0) {
+    res.send("Empty body")
+    return
+  }
+
+  // We assume we have a valid location entry here
+  // documentation on req body is here https://overland.p3k.app/
+  let locationEntries = req.body.locations
+
+  // {
+  //   type: 'Feature',
+  //       geometry: {
+  //   type: 'Point',
+  //       coordinates: [ -122.05361586868501, 36.97086197075715 ]
+  // },
+  //   properties: {
+  //     speed: 1,
+  //         battery_state: 'unplugged',
+  //         motion: [ 'walking' ],
+  //         timestamp: '2021-11-13T06:01:09Z',
+  //         battery_level: 1,
+  //         vertical_accuracy: 3,
+  //         pauses: false,
+  //         horizontal_accuracy: 5,
+  //         wifi: '',
+  //         deferred: 100,
+  //         significant_change: 1,
+  //         locations_in_payload: 1,
+  //         activity: 'other',
+  //         device_id: 'CDXS',
+  //         altitude: 75,
+  //         desired_accuracy: -1
+  //   }
+  // }
+  // Format of this object ^^
+  lastKnownLocationObject = locationEntries[locationEntries.length - 1]
+
+  // This response is necessary to flush the app's cache of locations from the batch it just sent
+  res.send({result: "ok"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
