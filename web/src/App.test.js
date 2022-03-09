99871fb881f0a0a0d6eecbef6a7a84d988e09e44
abiirami <abi2rami@gmail.com>
2022-03-08 18:56:31 -0800

    formatting

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -44,30 +44,31 @@ test("input task is displayed", () => {
 
 
 test("submit first task", async () => {
-    const { getByText} = render(<App />);
-   const button = getByText(/Lets get started/i);
- fireEvent.change(screen.getByPlaceholderText('Ex: 123-456-7891',"123-456-7891" )); 
- fireEvent.click(button);
- fireEvent.change(screen.getByPlaceholderText("Add new todo task","cry and fry" ));
- const emptyTodo = getByText(/Nothing to do!/i);
- const button3 = getByText(/Submit/i);
- fireEvent.click(button3);
-   await expect(emptyTodo).not.toBeInTheDocument();
+  const { getByText} = render(<App />);
+  const button = getByText(/Lets get started/i);
+  fireEvent.change(screen.getByPlaceholderText('Ex: 123-456-7891',"123-456-7891" )); 
+  fireEvent.click(button);
+  fireEvent.change(screen.getByPlaceholderText("Add new todo task","cry and fry" ));
+  const emptyTodo = getByText(/Nothing to do!/i);
+  const button3 = getByText(/Submit/i);
+  fireEvent.click(button3);
+  
+  await expect(emptyTodo).not.toBeInTheDocument();
  });
 
- test("delete last task", async () => {
-    const { getByText} = render(<App />);
-   const button = getByText(/Lets get started/i);
+test("delete last task", async () => {
+  const { getByText} = render(<App />);
+  const button = getByText(/Lets get started/i);
  
- fireEvent.change(screen.getByPlaceholderText('Ex: 123-456-7891',"123-456-7891" )); 
- fireEvent.click(button);
- fireEvent.change(screen.getByPlaceholderText("Add new todo task","cry and fry" ));
+  fireEvent.change(screen.getByPlaceholderText('Ex: 123-456-7891',"123-456-7891" )); 
+  fireEvent.click(button);
+  fireEvent.change(screen.getByPlaceholderText("Add new todo task","cry and fry" ));
  
- const button3 = getByText(/Submit/i);
- fireEvent.click(button3);
- const button4 = getByText(/✕/i);
+  const button3 = getByText(/Submit/i);
+  fireEvent.click(button3);
+  const button4 = getByText(/✕/i);
   fireEvent.click(button4);
   const emptyTodo = getByText(/Nothing to do!/i);
   
-   await expect(emptyTodo).toBeInTheDocument();
+  await expect(emptyTodo).toBeInTheDocument();
  });
